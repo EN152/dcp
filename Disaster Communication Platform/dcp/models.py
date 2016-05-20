ec92fb6dd8916bf673a347caa10f6b595b1485a1
Jasper Bernhardt <jasper@intra.bernhardt.onl>
2016-05-20 12:28:20 +0200

    fixed model unicode def

--- Disaster Communication Platform/dcp/models.py
+++ Disaster Communication Platform/dcp/models.py
@@ -6,6 +6,6 @@ class Catastrophe(models.Model):
 	cat_title = models.CharField(max_length=200)
 	cat_location = models.CharField(max_length=100)
 	cat_pub_date = models.DateTimeField('date published')
-
-    	def __unicode__(self):
-			return self.cat_title
+	
+	def __unicode__(self):
+		return self.cat_title
