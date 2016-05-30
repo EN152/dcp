690b267fee08208d201d9d47c390f8ebf5af390a
jasper <jasper@JASPERXPS12>
2016-05-30 22:23:05 +0200

    es wird besser

--- Disaster Communication Platform/travis.sh
+++ Disaster Communication Platform/travis.sh
@@ -2,7 +2,7 @@
 set -e # stops execution on error
 rm -rf "Disaster Communication Platform/dcp/migrations"
 rm -f "Disaster Communication Platform/db.sqlite3"
-python "Disaster Communication Platform/manage.py" makemigrations --noinput
+python "Disaster Communication Platform/manage.py" makemigrations --noinput dcp
 python "Disaster Communication Platform/manage.py" migrate --noinput
 python "Disaster Communication Platform/__travis/mkadmin.py"
 python "Disaster Communication Platform/manage.py" test
