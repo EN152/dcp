d7edd20d97f9d7bec7a07305ff55a2ef2cff8239
jasper <jasper@JASPERXPS12>
2016-05-30 21:32:04 +0200

    umbennung von zu alter version

--- Disaster Communication Platform/travis.sh
+++ Disaster Communication Platform/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e # stops execution on error
 rm -rf "Disaster Communication Platform/dcp/migrations"
-python "Disaster Communication Platform/manage.py" schemamigration dcp --initial
+python "Disaster Communication Platform/manage.py" makemigration --initial
 python "Disaster Communication Platform/manage.py" syncdb --noinput
 python "Disaster Communication Platform/manage.py" migrate dcp
 python "Disaster Communication Platform/__travis/mkadmin.py"
