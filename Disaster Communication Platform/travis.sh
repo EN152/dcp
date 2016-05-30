88e56ca1a5a97870bb5b6c26615a76f35596da91
jasper <jasper@JASPERXPS12>
2016-05-30 22:00:32 +0200

    und nochmal versuchen

--- Disaster Communication Platform/travis.sh
+++ Disaster Communication Platform/travis.sh
@@ -2,6 +2,6 @@
 set -e # stops execution on error
 rm -rf "Disaster Communication Platform/dcp/migrations"
 python "Disaster Communication Platform/manage.py" makemigrations
-python "Disaster Communication Platform/manage.py" migrate dcp
+python "Disaster Communication Platform/manage.py" migrate --run-syncdb --noinput
 python "Disaster Communication Platform/__travis/mkadmin.py"
 python "Disaster Communication Platform/manage.py" test
