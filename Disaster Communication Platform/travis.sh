d475e1dd339cf1ddebe0025e7d6850f9c8d4b30b
jasper <jasper@JASPERXPS12>
2016-05-30 22:14:28 +0200

    etwas mehr löschen

--- Disaster Communication Platform/travis.sh
+++ Disaster Communication Platform/travis.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e # stops execution on error
 rm -rf "Disaster Communication Platform/dcp/migrations"
-python "Disaster Communication Platform/manage.py" makemigrations
-python "Disaster Communication Platform/manage.py" migrate --run-syncdb --noinput
+rm -f "Disaster Communication Platform/db.sqlite3"
+python "Disaster Communication Platform/manage.py" makemigrations --noinput
+python "Disaster Communication Platform/manage.py" migrate --noinput
 python "Disaster Communication Platform/__travis/mkadmin.py"
 python "Disaster Communication Platform/manage.py" test
