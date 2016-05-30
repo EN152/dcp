b5f8e7ff1a32265723ff2c9786580b2e42d20071
jasper <jasper@JASPERXPS12>
2016-05-30 21:40:12 +0200

    try & error für postgres

--- Disaster Communication Platform/travis.sh
+++ Disaster Communication Platform/travis.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e # stops execution on error
 rm -rf "Disaster Communication Platform/dcp/migrations"
-python "Disaster Communication Platform/manage.py" makemigrations --initial
-python "Disaster Communication Platform/manage.py" syncdb --noinput
+python "Disaster Communication Platform/manage.py" makemigrations
 python "Disaster Communication Platform/manage.py" migrate dcp
 python "Disaster Communication Platform/__travis/mkadmin.py"
 python "Disaster Communication Platform/manage.py" test
