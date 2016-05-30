c60dedcc194ff388e2ce9b60ac828b40278cad63
jasper <jasper@JASPERXPS12>
2016-05-30 21:26:13 +0200

    etwas für die datenbank

---
+++ Disaster Communication Platform/__travis/mkadmin.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+
+u, created = User.objects.get_or_create(username='admin')
+if created:
+    u.set_password('password')
+    u.is_superuser = True
+    u.is_staff = True
+    u.save()
