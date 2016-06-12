4b867f49945fbd667782437f10f4b2fb3c22ffe8
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2016-06-12 19:17:17 +0200

    dcp/viewerClasses/people.py

--- Disaster Communication Platform/dcp/viewerClasses/people.py
+++ Disaster Communication Platform/dcp/viewerClasses/people.py
@@ -2,6 +2,6 @@ from dcp.importUrls import *
 
 class Suchen_Personen(View):
     template = 'dcp/content/suchen/personen.html'
-
+    
     def get(self, request):
-        return getPageAuthenticated(request, self.template)
+        return dcp.viewerClasses.authentication.getPageAuthenticated(request, self.template)
