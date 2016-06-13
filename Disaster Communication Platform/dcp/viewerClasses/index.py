d51f180791e740a57fbf04218df0c9d15b3e4c97
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2016-06-14 01:33:25 +0200

    Beginn einer dynamischen Startseite

--- Disaster Communication Platform/dcp/viewerClasses/index.py
+++ Disaster Communication Platform/dcp/viewerClasses/index.py
@@ -1,16 +1,36 @@
 # imports
 from dcp.importUrls import *
 
-
 class Index(View):
-    template = 'dcp/index.html'
 
-    def get(self, request):
-        return dcp.viewerClasses.authentication.getPageAuthenticated(request, self.template)
+	def get(self, request):
+		countSearch = -1
+		countOffer = -1
+		countInformation = -1
+		countPeople = len(MissedPeople.objects.all())
+		
+		# user = request.user und irgendwas wie .filter(user=user) fehlt noch...
+		goods_list = sorted(Goods.getAllGoods(), key=lambda g: g.created_date, reverse=True)
+		category_glyphicon_list = Categorys.getCategoryListAsGlyphiconString()
+		category_name_list = Categorys.getCategoryListAsNameString()
 
-    def post(self, request):
-        if request.user.is_authenticated():
-            params = {}
-            return render(request, 'dcp/index.html', params)
-        else:
-            return HttpResponseRedirect("anmelden/")
+		category_list = zip(category_glyphicon_list, category_name_list)
+
+		template = 'dcp/index.html'
+		params = {
+				'goods_list': goods_list,
+				'category_list' : category_list,
+				'countSearch': countSearch,
+				'countOffer': countOffer,
+				'countInformation': countInformation,
+				'countPeople': countPeople
+		}
+		return dcp.viewerClasses.authentication.getPageAuthenticated(request, template, params)
+
+	def post(self, request):
+		self.params += request.user
+
+		if request.user.is_authenticated():
+			return HttpResponseRedirect("")
+		else:
+			return HttpResponseRedirect("anmelden/")
