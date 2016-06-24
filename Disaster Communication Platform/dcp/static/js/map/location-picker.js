7b7e637ffd1266ac6a6bc95f3621cc3e84a18f58
jasper <jasper@JASPERXPS12>
2016-06-24 23:41:24 +0200

    Große Erweiterungen für Mappicks (Leaflet)

--- Disaster Communication Platform/dcp/static/js/map/location-picker.js
+++ Disaster Communication Platform/dcp/static/js/map/location-picker.js
@@ -1,41 +1,82 @@
-﻿function newMapScriptPicker(divMapId, valueLocationXId, valueLocationYId) {
+﻿var map;
+var marker;
+var circle;
+var valueLocationXId;
+var valueLocationYId;
+var radius = 0;
+
+function newMapScriptPicker(divMapId, valueLocationXId, valueLocationYId) {
     //set the start focus of the map
-    var map = L.map(divMapId).setView([37.804146, -122.275045], 15);
+    this.map = L.map(divMapId).setView([51.508, -0.11], 13);
+    this.valueLocationXId = valueLocationXId;
+    this.valueLocationYId = valueLocationYId;
     
     //contributors:
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
-    
+    map.on
     
     // Add geocoding plugin etc. search bar
     var options = {
         bounds: true,
         position: "topleft",
-        expanded: true
+        expanded: true,
+        markers: false
     }
     
     // add search key and options to the map
     var geocoder = L.control.geocoder('search-Hv2UM4h', options).addTo(map);
-    
     // when we select a city wich we searched for we are putting that into the 
     geocoder.on('select', function (e) {
-        console.log('highlight', e);
-        document.getElementById(valueLocationXId).value = e.latlng.lat
-        document.getElementById(valueLocationYId).value = e.latlng.lng
+        changeMapCreateMarker(e.latlng);
     });
     
     // if the map was able to find my current location
     map.on('locationfound', function (e) {
-        marker = L.marker([e.latitude, e.longitude], { draggable: false });
-        this.addLayer(marker);
-        document.getElementById(valueLocationXId).value = e.latitude;
-        document.getElementById(valueLocationYId).value = e.longitude;
+
+        changeMapCreateMarker(e.latlng);
     });
     
     // start the location tracker
 }
 function getCurrentLocation() {
-    var lc = L.control.locate().addTo(map);
-    lc.start();
+    map.locate({
+        setView: true});
+}
+function changeMapCreateMarker(latlng) {
+    if (marker != undefined) {
+        map.removeLayer(marker)
+    }
+    marker = L.marker(latlng, { draggable: true });
+    map.addLayer(marker);
+    changeValuesMapCreate(latlng);
+    changeCircleCreateNew();
+    marker.on('dragend', function (e) {
+        changeValuesMapCreate(marker.getLatLng());
+        changeCircleCreateNew();
+    });
+}
+function changeValuesMapCreate(latlng) {
+    document.getElementById(valueLocationXId).value = latlng.lat;
+    document.getElementById(valueLocationYId).value = latlng.lng;
+}
+function newMapCreateRender() {
+    map.invalidateSize();
+}
+function changeCircleCreateNew() {
+    if (this.circle != undefined) {
+        map.removeLayer(circle);
+    }
+
+    this.circle = L.circle(marker.getLatLng(), radius, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.3,
+        clickable: false
+    }).addTo(map);
+}
+function changeRadiusOnMap(radius) {
+    this.radius = 1000 * radius;
+    changeCircleCreateNew();
 }
